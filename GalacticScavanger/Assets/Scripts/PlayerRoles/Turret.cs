314cac1f1b33ea09394c82e5dcc24224bca93024
Gabriel O <oliver.gabriel.a@hotmail.com>
2023-05-03 15:05:28 -0700

    setting up turret prefab

--- GalacticScavanger/Assets/Scripts/PlayerRoles/Turret.cs
+++ GalacticScavanger/Assets/Scripts/PlayerRoles/Turret.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// code source: https://www.youtube.com/watch?v=bCz7awDbl58
 public class Turret : MonoBehaviour
 {
+    [SerializeField] private GameObject camera;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] public float rotationSpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Vector3 newRotation = Vector3.RotateTowards();
+        //this.gameObject.transform.rotation =
+    }
+/*
+    private void Update() 
+    {
+        Aim();
+        //weapon.fire();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Aim()
     {
+        // TURN
+        float targetPlaneAngle = vector3AngleOnPlane(target.position, transform.position, -transform.up, transform.forward);
+        Vector3 newRotation = new Vector3(0, targetPlaneAngle, 0);
+        transform.Rotate(newRotation, Space.Self);
         
+        // UP/DOWN
+        float upAngle = Vector3.Angle(target.position, barrel.transform.up);
+        Vector3 upRotation = new Vector3(-upAngle + 90, 0, 0);
+        barrel.transform.Rotate(upRotation, Space.Self);
     }
+
+    float vector3AngleOnPlane(Vector3 from, Vector3 to, Vector3 planeNormal, Vector3 toZeroAngle)
+    {
+        Vector3 projectedVector = Vector3.ProjectOnPlane(from - to, planeNormal);
+        float projectedVectorAngle = Vector3.SignedAngle(projectedVector, toZeroAngle, planeNormal);
+
+        return projectedVectorAngle;
+    } 
+    */
 }
