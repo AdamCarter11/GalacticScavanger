bd3be8567eadb10629f4be574dd3c4b45c1857f1
Gabriel O <oliver.gabriel.a@hotmail.com>
2023-05-11 20:42:37 -0700

    enemy has turret that shoots at enemy. need to prevent shooting if the player is too low

---
+++ GalacticScavanger/Assets/Scripts/Enemy/EnemyTurret.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float distanceToFire = 40f;
+    [SerializeField] private float fireDelaySeconds = 0.5f;
+    private float runningTime = 0;
+    private GameObject playerShip;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerShip = GameObject.FindWithTag("Ship");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerShip)
+        {
+            Vector3 enemyToPlayer = playerShip.transform.position - this.transform.position;
+            //this.transform.rotation = Quaternion.Euler(enemyToPlayer);
+            // Calculate the rotation needed to look in that direction
+            Quaternion targetRotation = Quaternion.LookRotation(enemyToPlayer);
+
+            // Apply the rotation gradually over time
+            float rotationSpeed = 100f; // Adjust as needed
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            
+            if (enemyToPlayer.magnitude <= distanceToFire && runningTime >= fireDelaySeconds)
+            {
+                FireWeapon();
+                runningTime = 0 - Time.deltaTime;
+            }
+            runningTime += Time.deltaTime;
+        }
+        else
+        {
+            playerShip = GameObject.FindWithTag("Ship");
+        }
+    }
+
+    private void FireWeapon()
+    {
+        Debug.Log("Firing Weapon");
+        Instantiate(projectile, this.transform.position, this.transform.rotation);
+    }
+}
--- GalacticScavanger/Assets/Scripts/Enemy/ProjectileBehavior.cs
+++ GalacticScavanger/Assets/Scripts/Enemy/ProjectileBehavior.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileBehavior : MonoBehaviour
 {
-    public float velocity = 1f;
+    public float velocity = 10f;
     [SerializeField] private float lifetimeSeconds = 3f;
     private float runningTime = 0;
     // Update is called once per frame
@@ -18,5 +19,14 @@ public class ProjectileBehavior : MonoBehaviour
         this.transform.position += this.transform.forward * velocity * Time.deltaTime;
 
         runningTime += Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ship"))
+        {
+            Debug.Log("enemy hit target");
+            Destroy(this.gameObject);
+        }
     }
 }
