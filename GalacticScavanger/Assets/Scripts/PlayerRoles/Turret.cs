4c8645d639ca16afd39a06cbf108c3f2d9581d90
Gabriel O <olivergabe@hotmail.com>
2023-05-15 20:17:25 -0700

    turret shoots spherecasts

--- GalacticScavanger/Assets/Scripts/PlayerRoles/Turret.cs
+++ GalacticScavanger/Assets/Scripts/PlayerRoles/Turret.cs
@@ -9,6 +9,16 @@ public class Turret : MonoBehaviour
     [Header("Turret Parts")]
     [SerializeField] private GameObject turretCamera;
     [SerializeField] private GameObject turretBarrelBase;
+    [SerializeField] private GameObject turretBarrel;
+    
+    [Header("Shooting Information")]
+    [SerializeField] public float fireRate = 0.1f;
+    [SerializeField] public int projectileDamage = 1;
+    [SerializeField] private float sphereCastRadius = 0.5f;
+    [SerializeField] private float sphereCastDistance = 100f;
+    private float runningTime = 0f;
+    [SerializeField] private LayerMask layerMaskToIgnore;
+    [SerializeField] private GameObject projectile;
 
     // ship information
     private GameObject ship;
@@ -32,6 +42,7 @@ public class Turret : MonoBehaviour
             turretLocation = ship.GetComponent<Ship>().turretLocation;
             this.transform.parent = turretLocation;
             this.transform.position = turretLocation.position;
+            runningTime = fireRate;
         }
         else
         {
@@ -44,7 +55,7 @@ public class Turret : MonoBehaviour
         if (ship)
         {
             RotationUpdate();
-            ShootingUpdate();
+            FireUpdate();
             //DebugViewingRays();
         }
         else
@@ -71,6 +82,66 @@ public class Turret : MonoBehaviour
         rollAngle = Mathf.Clamp(rollAngle, minAngle, maxAngle);
         turretBarrelBase.transform.eulerAngles = new Vector3(rollAngle, turretBarrelBase.transform.eulerAngles.y, turretBarrelBase.transform.eulerAngles.z);
     }
+
+    private void FireUpdate()
+    {
+        if (firePressed)
+        {
+            runningTime -= Time.deltaTime;
+        }
+        else
+        {
+            runningTime = 0;
+        }
+        
+        if (firePressed && runningTime <= 0)
+        {
+            runningTime = fireRate;
+            FiringHelper();
+        }
+    }
+
+    private void FiringHelper()
+    {
+        //Debug.Log("FireHelper");
+        //myRay = new Ray(turretCamera.transform.position, turretCamera.transform.forward);
+
+        /*Rect viewportRect = turretCamera.GetComponent<Camera>().rect;
+        Vector3 viewportCenter = new Vector3(viewportRect.x + viewportRect.width * 0.5f,
+            viewportRect.y + viewportRect.height * 0.5f, 0f);
+        //Ray ray = turretCamera.GetComponent<Camera>().ViewportPointToRay(viewportCenter);
+        myRay = turretCamera.GetComponent<Camera>().ViewportPointToRay(viewportCenter);
+        
+        RaycastHit hit;
+        //layerMask = LayerMask.GetMask("Default");
+        
+        if (Physics.Raycast(myRay, out hit, Mathf.Infinity, ~layerMaskToIgnore))
+        {
+            Debug.Log("Hit object: " + hit.transform.name);
+            //Instantiate(projectile, myRay.origin, Quaternion.Euler(myRay.direction));
+            // Do something with the hit object here
+        }*/
+        
+        
+        RaycastHit[] hits = Physics.SphereCastAll(turretBarrel.transform.position, sphereCastRadius, turretBarrel.transform.forward, sphereCastDistance, ~layerMaskToIgnore);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.transform.CompareTag("Enemy"))
+            {
+                Debug.Log("Hit an enemy!");
+            }
+            else
+            {
+                Debug.Log("Hit object: " + hit.transform.name);
+            }
+            // Do something with the hit object here
+        }
+        // Draw a debug line showing the direction and distance of the spherecast
+        Debug.DrawRay(turretBarrel.transform.position, turretBarrel.transform.forward * sphereCastDistance, Color.yellow, 1f);
+        
+        Instantiate(projectile, turretBarrel.transform.position, Quaternion.Euler(turretBarrel.transform.eulerAngles));
+        
+    }
     
     private void DebugViewingRays()
     {
@@ -82,11 +153,6 @@ public class Turret : MonoBehaviour
         Debug.DrawRay(ship.transform.position, ship.transform.forward*10, Color.blue);
     }
 
-    private void ShootingUpdate()
-    {
-        
-    }
-    
     #region Input Methods
     public void OnPitchYaw(InputAction.CallbackContext context)
     {
@@ -97,7 +163,7 @@ public class Turret : MonoBehaviour
     public void OnFire(InputAction.CallbackContext context)
     {
         firePressed = context.performed;
-        Debug.Log("in OnFire:" + firePressed);
+        //Debug.Log("in OnFire:" + firePressed);
     }
     #endregion
 }
