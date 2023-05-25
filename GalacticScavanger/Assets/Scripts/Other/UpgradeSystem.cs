397a89e061b5960d6d6c494010e0cb1d6c88a0ae
Jude Key <jakey@ucsc.edu>
2023-05-25 16:40:16 -0700

    Upgrade system cont

--- GalacticScavanger/Assets/Scripts/Other/UpgradeSystem.cs
+++ GalacticScavanger/Assets/Scripts/Other/UpgradeSystem.cs
@@ -14,24 +14,40 @@ public class UpgradeSystem : MonoBehaviour
     [SerializeField] float upThrustBonus = 1.1f;      // up/down speed
     [SerializeField] float strafeThrustBonus = 1.1f;  // left/right speed
     //controls accel/deccel (also what makes the ship eventually stop after they release the key
-    [SerializeField, Range(0.001f, 0.999f)] float thrustGlideReductionBonus = 1.1f;
-    [SerializeField, Range(0.001f, 0.999f)] float upDownGlideReductionBonus = 1.1f;
-    [SerializeField, Range(0.001f, 0.999f)] float leftRightGlideReductionBonus = 1.1f;
+    //[SerializeField, Range(0.001f, 0.999f)] float thrustGlideReductionBonus = 1.1f;
+    //[SerializeField, Range(0.001f, 0.999f)] float upDownGlideReductionBonus = 1.1f;
+    //[SerializeField, Range(0.001f, 0.999f)] float leftRightGlideReductionBonus = 1.1f;
+    [Header("Ship Turret Upgrade Percentages")]
+    [SerializeField] public float fireRateBonus = 0.9f;
+    [SerializeField] public int projectileDamageBonus = 1;
+    [SerializeField] public int healthBonus = 1;
 
     private bool canBeUpgraded = false;
     private int upgradeLevel = 1;
 
+    int whichClass1;
+    int whichClass2;
 
     public void Upgrade()
     {
+        whichClass1 = PlayerPrefs.GetInt("Player1Character");
+        whichClass2 = PlayerPrefs.GetInt("Player2Character");
+
+        //engine upgrades
         GetComponent<Ship>().yawTorque *= yawTorqueBonus;
         GetComponent<Ship>().pitchTorque *= pitchTorqueBonus;
         GetComponent<Ship>().rollTorque *= rollTorqueBonus;
+
         GetComponent<Ship>().thrust *= thrustBonus;
         GetComponent<Ship>().upThrust *= upThrustBonus;
         GetComponent<Ship>().strafeThrust *= strafeThrustBonus;
         //GetComponent<Ship>().thrustGlideReduction *= thrustGlideReductionBonus;
         //GetComponent<Ship>().upDownGlideReduction *= upDownGlideReductionBonus;
         //GetComponent<Ship>().leftRightGlideReduction *= leftRightGlideReductionBonus;
+
+        //gun upgrades
+        GetComponent<Ship>().health += healthBonus;
+        GetComponent<Turret>().fireRate *= fireRateBonus;
+        GetComponent<Turret>().projectileDamage += projectileDamageBonus;
     }
 }
